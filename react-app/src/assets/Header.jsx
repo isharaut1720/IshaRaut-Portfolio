9d23ddc492e145c52a55ed58bfd0b483f17262ad
isharaut1720 <ishaaraut65@gmail.com>
2025-08-14 00:19:11 +0530

    Update header

--- react-app/src/assets/Header.jsx
+++ react-app/src/assets/Header.jsx
@@ -3,7 +3,7 @@ function Header() {
 
     return(
         <header className = "header">
-            <h1>Isha Raut</h1>
+            <h1>Isha's Portfolio</h1>
             <p>Aspiring Software Engineer | Java | Python | Web Development</p>
         </header>
     )
